d56ec6ec3c11bb68c4d4bc8b8e44990fe494789b
MUEH091 <ante.muehlbock@bertelsmann.de>
2025-11-19 13:41:50 +0100

    Created Datamodel for MoonPhases and fetching data from api

--- RocketMoonApp.Server/Models/MoonPhase.cs
+++ RocketMoonApp.Server/Models/MoonPhase.cs
@@ -2,9 +2,7 @@ namespace RocketMoonApp.Server.Models
 {
     public class MoonPhase
     {
-        public int Id { get; set; } = 0;
         public string PhaseName { get; set; } = string.Empty;
-        public DateTimeOffset Date { get; set; }
-        public Location Location { get; set; } = new Location();
+        public DateTime Date { get; set; }
     }
 }
